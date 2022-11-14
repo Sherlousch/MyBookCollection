629a14047fb3220782476dff7c70cd0af5ca9680
Lousch <schnee.louise@gmail.com>
2022-11-14 22:10:21 +0100

    before TP6

--- src/Controller/Admin/BookcollectionCrudController.php
+++ src/Controller/Admin/BookcollectionCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Bookcollection;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -20,8 +23,18 @@ class BookcollectionCrudController extends AbstractCrudController
         return [
             TextField::new('description'),
             AssociationField::new('membre'),
-            AssociationField::new('books')->onlyOnDetail(),
+            AssociationField::new('books')
+                ->onlyOnDetail()
+                ->setTemplatePath('admin/fields/bookcollection_books.html.twig'),
             IdField::new('id')->hideOnForm(),
         ];
     } 
+
+    public function configureActions(Actions $actions): Actions
+    {
+
+    return $actions
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+    ;
+    }
 }
