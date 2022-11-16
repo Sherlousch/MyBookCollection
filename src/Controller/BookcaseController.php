70a9fb483722f1879df6fc943f940dacf5cf1f72
Lousch <schnee.louise@gmail.com>
2022-11-16 22:55:33 +0100

    TP7 step 1 done

---
+++ src/Controller/BookcaseController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Bookcase;
+use App\Form\BookcaseType;
+use App\Repository\BookcaseRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/bookcase')]
+class BookcaseController extends AbstractController
+{
+    #[Route('/', name: 'app_bookcase_index', methods: ['GET'])]
+    public function index(BookcaseRepository $bookcaseRepository): Response
+    {
+        return $this->render('bookcase/index.html.twig', [
+            'bookcases' => $bookcaseRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_bookcase_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, BookcaseRepository $bookcaseRepository): Response
+    {
+        $bookcase = new Bookcase();
+        $form = $this->createForm(BookcaseType::class, $bookcase);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $bookcaseRepository->save($bookcase, true);
+
+            return $this->redirectToRoute('app_bookcase_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('bookcase/new.html.twig', [
+            'bookcase' => $bookcase,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_bookcase_show', methods: ['GET'])]
+    public function show(Bookcase $bookcase): Response
+    {
+        return $this->render('bookcase/show.html.twig', [
+            'bookcase' => $bookcase,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_bookcase_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Bookcase $bookcase, BookcaseRepository $bookcaseRepository): Response
+    {
+        $form = $this->createForm(BookcaseType::class, $bookcase);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $bookcaseRepository->save($bookcase, true);
+
+            return $this->redirectToRoute('app_bookcase_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('bookcase/edit.html.twig', [
+            'bookcase' => $bookcase,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_bookcase_delete', methods: ['POST'])]
+    public function delete(Request $request, Bookcase $bookcase, BookcaseRepository $bookcaseRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$bookcase->getId(), $request->request->get('_token'))) {
+            $bookcaseRepository->remove($bookcase, true);
+        }
+
+        return $this->redirectToRoute('app_bookcase_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/BookcaseType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Bookcase;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookcaseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('description')
+            ->add('released')
+            ->add('owner')
+            ->add('books')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Bookcase::class,
+        ]);
+    }
+}
